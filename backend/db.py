343183fdef87b95c217f264b545ddc5fe33fb876
Satwik <veluvalisaisatwik7@gmail.com>
2026-06-01 15:01:51 +0530

    connected render database

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,22 @@
 import psycopg2
 
-def db_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="dvs",
-        user="postgres",
-        password="1234"
-    )
+db_host = "dpg-d8el10l7vvec73dpnfjg-a"
+db_user = "docverify_user"
+db_password = "u4fR0KOrvJA4FITyAbzrUBOs7HeP9SVD"
+db_port = "5432"
+db_name = "docverify_tskd"
 
-def return_db_connection(conn):
-    if conn:
-        conn.close()
+def db_connection():
+    try:
+        connection = psycopg2.connect(
+            host=db_host,
+            user=db_user,
+            password=db_password,
+            dbname=db_name,
+            port=db_port
+        )
+        return connection
+
+    except Exception as e:
+        print("Database connection failed:", e)
+        return None
